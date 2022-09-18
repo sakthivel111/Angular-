82517efe5235ef4641037f821cc413af09ca4228
sakthivel111 <sakthivelidn@gmail.com>
2022-09-18 07:39:32 +0530

    To Way Bainding Task

---
+++ AngularExperience/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TwoWayBindingComponent } from './two-way-binding/two-way-binding.component';
+import { FormsModule } from '@angular/forms';
+@NgModule({
+  declarations: [
+    AppComponent,
+    TwoWayBindingComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule 
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ AngularExperience/src/app/two-way-binding/two-way-binding.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-two-way-binding',
+  templateUrl: './two-way-binding.component.html',
+  styleUrls: ['./two-way-binding.component.scss']
+})
+export class TwoWayBindingComponent implements OnInit {
+  //step1
+  user: any;
+  //step2 
+  user_name: string = '';
+  disable: boolean = true;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
+  clearInput() {
+    this.user_name = '';
+    this.disable = true;
+  }
+  disablebutton(event: any) {
+    if (event.target.value =='') {
+      this.disable = true;
+    } else {
+      
+      this.disable = false;
+    }
+  }
+
+}
