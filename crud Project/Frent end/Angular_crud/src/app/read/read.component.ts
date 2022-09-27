37246faa95fe3e03051dd7324d91f948a183748a
sakthivel111 <sakthivelidn@gmail.com>
2022-09-27 17:51:27 +0530

    Angular crud

--- crud Project/Frent end/Angular_crud/src/app/read/read.component.ts
+++ crud Project/Frent end/Angular_crud/src/app/read/read.component.ts
@@ -1,21 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiserviceService } from '../apiservice.service';
+import { ActivatedRoute, Router } from '@angular/router';
+//import { AppService } from '../app.service';
 @Component({
   selector: 'app-read',
   templateUrl: './read.component.html',
   styleUrls: ['./read.component.scss']
 })
 export class ReadComponent implements OnInit {
-  data:any;
-  constructor(private service:ApiserviceService) { }
-  
+  data: any;
+  id: any;
+
+  constructor(private service: ApiserviceService, private router: Router) { }
 
   ngOnInit(): void {
-    this .service.getall().subscribe((res)=>{
-      console.log(res,'respone')
-      this.data=res
-      console.log(this.data,"data taaaaaa")
+    this.service.getall().subscribe((res) => {
+      console.log(res, 'respone')
+      this.data = res
+      //console.log(this.data,"data taaaaaa")
     })
   }
 
-} 
+  edit(userId: any) {
+
+    console.log('hfhfhhf', userId)
+
+    //  this.router.navigate(['edit',userId],{relativeTo:this.aRoute})
+    this.router.navigate(['creat'], { queryParams: { userId} })
+  }
+
+
+
+}
