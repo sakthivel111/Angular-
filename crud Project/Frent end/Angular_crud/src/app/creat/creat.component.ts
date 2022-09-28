ad42ea474f84ac306706a12ae963d42b39d28140
sakthivel111 <sakthivelidn@gmail.com>
2022-09-28 11:53:50 +0530

    Angular crud

--- crud Project/Frent end/Angular_crud/src/app/creat/creat.component.ts
+++ crud Project/Frent end/Angular_crud/src/app/creat/creat.component.ts
@@ -8,39 +8,63 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./creat.component.scss']
 })
 export class CreatComponent implements OnInit {
-username:any;
-userage:any;
-userjob:any;
-userId :any;
-  constructor( private service:ApiserviceService,private router:Router,private aRoute:ActivatedRoute) { 
+  update: any = false;
+  changingname: string = 'submit';
+  username: any;
+  userage: any;
+  userjob: any;
+  userId: any;
+  bainding: any
+  delet:any;
+  constructor(private service: ApiserviceService, private router: Router, private aRoute: ActivatedRoute) {
+    //appi using method
     aRoute.queryParams.subscribe(params => {
       this.userId = params['userId'];
-      console.log('QQQQQQQQQQQQQQ', this.userId)
-      this.service.getId( this.userId ).subscribe((res) => {
-        console.log(res, 'respone')
+      //console.log('QQQQQQQQQQQQQQ', this.userId);
+      this.service.getId(this.userId).subscribe((res: any) => {
+        // console.log(res, 'respone')
+        this.update = true;
+        this.changingname = 'update';
+        this.userId = res[0].id;
+        this.username = res[0].name;
+        this.userage = res[0].age;
+        this.userjob = res[0].job;
       })
-  });
-  
-//1) method 
-// aRoute.queryParams.subscribe(params => {
-//     this.userId = params['id'];
-//     this.username = params['name'];
-//     this.userage = params['age'];
-//     this.userjob = params['job'];
-//     console.log('QQQQQQQQQQQQQQ', this.userId)
-// })
+    });
+
+    aRoute.queryParams.subscribe(params => {
+      this.delet = params['deletId'];
+
+    })
+
+    //1) method  (not use for api)
+    // aRoute.queryParams.subscribe(params => {
+    //     this.userId = params['id'];
+    //     this.username = params['name'];
+    //     this.userage = params['age'];
+    //     this.userjob = params['job'];
+    //     console.log('QQQQQQQQQQQQQQ', this.userId)
+    // })
 
   }
 
   ngOnInit(): void {
-    
+
   }
-  adduser(){
-    //console.log({name:this.username,age:this.userage,job:this.userjob}, 'ssssss')
-    this.service.adduser({name:this.username,age:this.userage,job:this.userjob}).subscribe((newUser)=>{
-     // console.log(newUser)
-      this.router.navigate(['/read'])
-    })
+ 
+  adduser() {
+    if (this.update == false) {
+      //console.log({name:this.username,age:this.userage,job:this.userjob}, 'ssssss')
+      this.service.addinguser({ name: this.username, age: this.userage, job: this.userjob }).subscribe((newUser) => {
+        // console.log(newUser)
+        this.router.navigate(['/read'])
+      })
+    } else {
+      this.service.edituser({ id: this.userId, name: this.username, age: this.userage, job: this.userjob }).subscribe((newUser) => {
+        // console.log(newUser)
+        this.router.navigate(['/read'])
+      })
+    }
   }
 
 }
